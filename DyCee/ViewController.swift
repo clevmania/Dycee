a1d3226c5a4e6802566ea596860259aa2c151501
clevmania <ernestcleverest@yahoo.com>
2020-07-05 15:50:42 +0100

    feat: implement roll dice logic

--- DyCee/ViewController.swift
+++ DyCee/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let addDice = [#imageLiteral(resourceName: "dice1") ,#imageLiteral(resourceName: "dice2") , #imageLiteral(resourceName: "dice3"), #imageLiteral(resourceName: "dice4"), #imageLiteral(resourceName: "dice5"), #imageLiteral(resourceName: "dice6")]
+        
+        diceImageViewOne.image = addDice[Int.random(in: 0...5)]
+        diceImageViewTwo.image = addDice[Int.random(in: 0...5)]
+    }
 }
 
